b761950acaafdfb4fcde949b5a75e89617225e73
Anubhav Kumar Singh <113932642+Anubhav2912@users.noreply.github.com>
2025-06-04 14:56:21 +0530

    Add files via upload

---
+++ src/test/java/com/epics82/agrovita/AgrovitaApplicationTests.java
@@ -0,0 +1,8 @@
+package com.epics82.agrovita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrovitaApplicationTests {
+}
